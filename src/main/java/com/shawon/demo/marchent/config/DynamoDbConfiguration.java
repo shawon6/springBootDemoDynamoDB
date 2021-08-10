0c508cf226921141015d117f95ded8bbb098b946
shawon6 <shawon@i2gether.com>
2021-08-10 13:33:58 +0600

    app properties resolve

--- src/main/java/com/shawon/demo/marchent/config/DynamoDbConfiguration.java
+++ src/main/java/com/shawon/demo/marchent/config/DynamoDbConfiguration.java
@@ -1,41 +1,73 @@
 package com.shawon.demo.marchent.config;
 
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.util.StringUtils;
 
 @Configuration
+@EnableDynamoDBRepositories(basePackages = "com.baeldung.spring.data.dynamodb.repositories")
 public class DynamoDbConfiguration {
 	
-	@Value("${aws.region}")
-	public String aws_region;
+//	@Value("${aws.region}")
+//	public String aws_region;
+//	
+//	@Value("${service.endpoint}")
+//	public String service_endpoint;
+//	
+//	@Bean
+//	public DynamoDBMapper dynamoDBMapper() {
+//		return new DynamoDBMapper(buildAmazonDynamoDB());
+//	}
+//
+//	private AmazonDynamoDB buildAmazonDynamoDB() { 
+//		return  AmazonDynamoDBClientBuilder
+//				.standard()
+//				.withEndpointConfiguration(
+//						new AwsClientBuilder.EndpointConfiguration(
+//								"http://localhost:8020", 
+//								"ap-southeast-1"
+//						)
+//				)
+//				.build();
+//				
+//	}
 	
-	@Value("${service.endpoint}")
-	public String service_endpoint;
-	
-	@Bean
-	public DynamoDBMapper dynamoDBMapper() {
-		return new DynamoDBMapper(buildAmazonDynamoDB());
-	}
+	@Value("${amazon.dynamodb.endpoint}")
+    private String amazonDynamoDBEndpoint;
 
-	private AmazonDynamoDB buildAmazonDynamoDB() { 
-		return  AmazonDynamoDBClientBuilder
-				.standard()
-				.withEndpointConfiguration(
-						new AwsClientBuilder.EndpointConfiguration(
-								"http://mapp-localstack:4569", 
-								"ap-southeast-1"
-						)
-				)
-				.build();
-				
-	}
+    @Value("${amazon.aws.accesskey}")
+    private String amazonAWSAccessKey;
+
+    @Value("${amazon.aws.secretkey}")
+    private String amazonAWSSecretKey;
+
+    @Bean
+    public AmazonDynamoDB amazonDynamoDB() {
+        AmazonDynamoDB amazonDynamoDB 
+          = new AmazonDynamoDBClient(amazonAWSCredentials());
+        
+        if (!StringUtils.isNullOrEmpty(amazonDynamoDBEndpoint)) {
+            amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint);
+        }
+        
+        return amazonDynamoDB;
+    }
+
+    @Bean
+    public AWSCredentials amazonAWSCredentials() {
+        return new BasicAWSCredentials(
+          amazonAWSAccessKey, amazonAWSSecretKey);
+    }
 }
